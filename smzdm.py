4072beed476047a3dcf17c930682abed852a5bbc
RoRyou <luliang_penn@live.com>
2020-12-07 17:00:14 +0800

    Update smzdm.py

--- smzdm.py
+++ smzdm.py
@@ -2,14 +2,14 @@
 import requests
 import random
 import re
-import time,datetime
+import time, datetime
 import calendar
 import pandas as pd
 
-keyword = '洗衣机'
+keyword = '鞋'
 time = datetime.datetime.now()
-lasttime = time -datetime.timedelta(days=3)
-refresh_time = 3
+lasttime = time - datetime.timedelta(days=3)
+refresh_time = 10000
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
@@ -17,32 +17,26 @@ r = requests.get('http://feed.smzdm.com', headers=headers, timeout=random.random
 
 title = re.findall(r'<title>(.*?)</title>', r.text)
 link = re.findall(r'<link>(.*?)</link>', r.text)
-pubDate =re.findall(r'<pubDate>(.*?)</pubDate>', r.text)
+pubDate = re.findall(r'<pubDate>(.*?)</pubDate>', r.text)
 
-output_data,output_link = [],[]
+output_data, output_link = [], []
 for n in range(refresh_time):
     for n in title:
-        if keyword in n:
-            #title.index(n)   为编号
+        if keyword in n:  # title.index(n)为编号
             D_time, m_time, Y_time, H_time, M_time, S_time = pubDate[1][14:16], pubDate[1][17:20], pubDate[1][21:25], \
                                                              pubDate[1][26:28], pubDate[1][29:31], pubDate[1][32:34],
             m_time = list(calendar.month_abbr).index(m_time)
-            thistime = datetime.datetime(int(Y_time),m_time,int(D_time))
-            print('-----')
+            thistime = datetime.datetime(int(Y_time), m_time, int(D_time))
             if lasttime < thistime:
-                print(n[9:-3])
-                #print(title.index(n))
-                print(link[title.index(n)][17:-3])
                 output_data.append(n[9:-3])
+
                 output_link.append(link[title.index(n)][17:-3])
 
+output = list(zip(output_data, output_link))
 
-print(output_data)
-output_data = pd.DataFrame(output_data)
-output_link = pd.DataFrame(output_link)
-
-output = output_data+output_link
+output = list(set(output))
+# print(output)
 
 
-
-
+for n in output:
+    print(n)
